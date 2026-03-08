7543f7b4bc7a34b477103177d21660dfe0ee2df8
odinaka-dev <uchechukwupeter22@gmail.com>
2026-03-08 02:27:38 +0100

    sample replay

--- src/main.tsx
+++ src/main.tsx
@@ -40,7 +40,7 @@ Sentry.init({
     /^https:\/\/agroheal\.solutions\/.com/,
   ],
   tracesSampleRate: 1.0,
-  replaysSessionSampleRate: 0.1,
+  replaysSessionSampleRate: 1.0,
   replaysOnErrorSampleRate: 1.0,
   sendDefaultPii: true,
 });
