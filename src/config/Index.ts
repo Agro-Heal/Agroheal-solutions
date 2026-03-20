b13920e51359da29cc58202270cb27e913fccdaf
odinaka-dev <uchechukwupeter22@gmail.com>
2026-03-20 13:47:33 +0100

    Payment changed from paystack to flutterwave

--- src/config/Index.ts
+++ src/config/Index.ts
@@ -3,5 +3,5 @@ export const supabaseURL = import.meta.env.VITE_SUPABASE_URL as string;
 export const supabaseANON = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
 
 // importing Payment secret keys
-export const PAYSTACK_KEY = import.meta.env.PAYSTACK_PUBLIC_KEYS;
-export const FLUTTERWAVE_KEYS = import.meta.env.FLUTTERWAVE_PUBLIC_KEYS;
+export const PAYSTACK_KEY = import.meta.env.VITE_PAYSTACK_PUBLIC_KEYS;
+export const FLUTTERWAVE_KEYS = import.meta.env.VITE_FLUTTERWAVE_PUBLIC_KEY;
