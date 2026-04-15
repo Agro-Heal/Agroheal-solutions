1df743815a38f01522289714064094c2b2a2c185
Binoculars7 <elitespot.com@gmail.com>
2026-04-15 17:08:48 +0100

    UI fix

--- src/page/website/Login.tsx
+++ src/page/website/Login.tsx
@@ -195,7 +195,7 @@ const Login = () => {
             </div>
           </motion.form>
 
-          {/* Divider */}
+          {/* Divider  */}
           <motion.div {...fadeUp(0.3)} className="flex items-center gap-3 my-6">
             <div className="flex-1 h-px bg-gray-200" />
             <span className="text-xs text-gray-400 font-medium">or</span>
